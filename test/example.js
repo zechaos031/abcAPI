dbd96e63b629381643149df540a0ddb0f35ac477
zechaos031 <36123003+zechaos031@users.noreply.github.com>
2020-02-08 16:41:02 +0100

    Update example.js

--- test/example.js
+++ test/example.js
@@ -1,11 +1,13 @@
-const abcAPI = require('abcapi');
+const abcAPI = require('../src/index');
 
 const { Client } = require('discord.js'),
     client = new Client();
 
-client.login('Bot TOKEN');
+client.login(BotToken);
 
 client.on('ready',() =>{
-    abcAPI.login('API Key', client.user.id);
-    abcAPI.post(client.guilds.size, client.users.size, "Number of shards");
+    abcAPI.login(AbcToken, client.user.id); // => require to use this module, log in to abcapi
+    abcAPI.update(client); //=> made update every minutes
+    abcAPI.post(client); //made simple post to this abcapi
+
 });
